700c70cecf9c433cc3a9a1ce88d685b6d2d6185e
HoverHell <hoverhell@gmail.com>
2017-10-26 17:14:40 +0300

    pyauxm packaging

---
+++ _newrelease.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+set -e
+set -x
+
+bumpversion minor
+
+git push --tags
+
+# # No testing that makes sense, currently. Maybe later.
+# tox
+
+python setup.py sdist upload
+python setup.py bdist_wheel upload
